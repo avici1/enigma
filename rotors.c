365f95c29632cced7448583e835ff0e65f3aa0ec
avici1 <alainchristian87@gmail.com>
2023-03-16 15:57:54 +0000

    Added new code

--- rotors.c
+++ rotors.c
@@ -1,44 +1,50 @@
 #include <stdio.h>
 
 int startPosition = 0;
-char alphabets [25] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+char alphabets [25] = {"ABCDEFGHIJKLMNOPQRSTUVWXYZ"};
+char rotors [5][26] = {
+    "EKMFLGDQVZNTOWYHXUSPAIBRCJ",
+    "AJDKSIRUXBLHWTMCQGZNPYFVOE",
+    "BDFHJLCPRTXVZNYEIWGAKMUSQO",
+    "ESOVPZJAYQUIRHXLNFTGKDCMWB",
+    "VZBRGITYUPSDNHLXAWMJQOFECK"
+    };
+
 int ringPosition = 20;
 int ringPositionForDecryption = 20;
 
-int CalculateEncryptedNewIndex(int toEncrypt) {
-for (int i=0; i<25; i++){
-        if (alphabets[i] == toEncrypt) {
-            int newIndex = i + ringPosition;
-            ringPosition +=1;
-            if (newIndex > 25) {
-                newIndex = newIndex - 25;
-                return newIndex;
-            }
-            return newIndex;
+/// @brief uses the given letter and finds it's index before returning the character representing the mapping
+/// @param entryLetter: char
+/// @return New integer value representing the position of the encrypted character.
+char rotorEntrySubstitution(char entryLetter, int rotorNumber) {
+
+for (int j=0; j<25; j++){
+    char currentLetter = rotors[rotorNumber][j];
+    char currentAlphabet = alphabets[j];
+    char selectedLetter = entryLetter;
+    char index = j;
+        if (alphabets[j] == entryLetter) {
+            // int newIndex = i + ringPosition;
+            // ringPosition +=1;
+            // if (newIndex > 25) {
+            //     newIndex = newIndex - 25;
+            //     return newIndex;
+            // }
+            // return newIndex;
+            char newValue = rotors[rotorNumber][j];
+            int x = 0;
+            return rotors[rotorNumber][j];
         }
     }
 }
 
-int CalculateDecryptedNewIndex(int toDecrypt) {
-    for (int i=0; i<25; i++){
-        if (alphabets[i] == toDecrypt) {
-            int newIndex = ringPositionForDecryption - i;
-            ringPositionForDecryption -=1;
-            if (newIndex > 25) {
-                newIndex = newIndex - 25;
-                return newIndex;
-            }
-            return newIndex;
-        }
-    }
-}
 int main() {
    for (int i = 0; i<5; i++) {
     char encrypt = 0;
-    printf("\n Enter a value\n");
+    printf("Enter a value\n");
     scanf("%c", &encrypt);
-    int newIndex = CalculateEncryptedNewIndex(encrypt);
-    printf("\n %c", alphabets[newIndex]);
+    char newChar = rotorEntrySubstitution(encrypt, 1);
+    printf("%c", newChar);
    }
     return 0;
 }
